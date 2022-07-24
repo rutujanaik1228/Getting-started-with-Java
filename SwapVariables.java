89939d23a18985086b80b0fefc9fdf3109df7788
Rutuja Naik <83773954+rutujanaik1228@users.noreply.github.com>
2022-07-24 22:05:14 +0530

    Add files via upload

---
+++ SwapVariables.java
@@ -0,0 +1,11 @@
+class SwapVariables
+{
+public static void main(String args[]){
+int a=10, b=20;
+int c;
+c=a;
+a=b;
+b=c; 
+System.out.println("a="+a);
+System.out.println("b="+b);
+}}
---
+++ Swapusingmulti.java
@@ -0,0 +1,12 @@
+class Swapusingmulti
+{
+public static void main(String arg[])
+{
+int a=10,b=20;
+b=a*b;
+a=b/a;
+b=b/a;
+System.out.println("a="+a);
+System.out.println("b="+b);}}
+
+
