8f771a314ffc0aafd9e096f302c380ad2c50a21f
Rutuja Naik <83773954+rutujanaik1228@users.noreply.github.com>
2022-07-24 22:03:22 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,21 @@
+class Variable
+{
+public static void main(String args[])
+{
+byte a=1;
+int b=10;
+short c=25256;
+long d=848685488565161513L;
+char e='R';
+boolean f=false;
+float g=23.667f;
+double h=87689.87897;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+}}
---
+++ VariablesDemo.java
@@ -0,0 +1,13 @@
+class VariablesDemo
+{
+ public static void main(String args[])
+{
+   /*int a; //declaration
+int a=10;// initialization*/
+// int a;
+//System.out.println(a); //gives error: variable a might not have been initialized
+int a=10,b=20;
+System.out.println("a="+a);
+System.out.println("b="+b);
+}
+}
